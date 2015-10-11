16f46509462a6556779a128cfbd7577b6a19bc70
sipida <sipida@gmail.com>
2015-10-11 15:01:01 -0700

    Create mobile-config.js

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+App.info({
+  name: 'HaoFumu',
+  description: 'An Android app help to do positive parenting',
+  version: '0.0.1'
+});
+
+App.icons({
+  'android_ldpi': 'resources/icons/icon-ldpi.png',
+  'android_mdpi': 'resources/icons/icon-mdpi.png',
+  'android_hdpi': 'resources/icons/icon-hdpi.png',
+  'android_xhdpi': 'resources/icons/icon-xhdpi.png'
+});
